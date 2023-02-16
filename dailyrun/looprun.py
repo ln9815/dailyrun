be7106f07c0e6b194439f8a8c2c7cf9e3bbfbc52
victor yang <mingxiangy@126.com>
2023-02-16 22:29:23 +0800

    add readme

--- dailyrun/looprun.py
+++ dailyrun/looprun.py
@@ -3,15 +3,15 @@
 
 import datetime
 import os
-import click
 import time
-import schedule
 import sys
+import click
+import schedule
 
 
 @click.command()
 @click.option('--cmd', '-c', type=str, required=True, help='command to run.')
-@click.option('--times', '-t', type=str, required=True, help='times to run command. valid format is HH:MM(:SS) and combined with %')
+@click.option('--times', '-t', type=str, required=True, help='times to run command. valid format is HH:MM(:SS) and combined with &')
 def run(cmd, times):
     def fn(cmd):
         print('{}: start to run command: {}.'.format(
@@ -19,9 +19,11 @@ def run(cmd, times):
         os.system(cmd)
         print('{}: end of command: {}.'.format(
             datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), cmd))
-    for t in times.split('%'):
+    
+    for t in times.split('&'):
         schedule.every().day.at(t).do(fn, cmd)
         print(f'command: {cmd} on: {t} added to list.')
+    
     while True:
         schedule.run_pending()
         time.sleep(1)
